d67cbda973fccf99816bd954f2bab7efc54520bf
ebi2kh <111044086+ebi2kh@users.noreply.github.com>
2024-04-27 15:57:11 +0330

    add big slider

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -3,6 +3,7 @@ import SimpleSlider from "../../components/slides/SimpleSlider";
 import SimpleBanner1 from "../../components/Static/SimpleBanner1";
 import OffSlider from "../../components/slides/OffSlider";
 import ProductSlider from "../../components/slides/ProductSlider";
+import BigSlider from "../../components/slides/BigSlider";
 
 function HomePage() {
   return (
@@ -11,6 +12,7 @@ function HomePage() {
       <SimpleBanner1 />
       <OffSlider />
       <ProductSlider />
+      <BigSlider />
     </>
   );
 }
